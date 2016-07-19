7f7c445ae56472f5b11db54b4a2217e2312210d6
Niels <ngribskov@gmail.com>
2016-07-19 15:22:12 -0400

    fix sign_up maybe, repair dashboard style

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,19 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
     @user.first_name = params['user']['first_name']
     @user.last_name = params['user']['last_name']
-
-    respond_to do |format|
-      if @user.save!
-        # Tell the UserMailer to send a welcome email after save
-        UserMailer.welcome_email(@user).deliver_later
-
-        format.html { redirect_to(@user, notice: 'User was successfully created.') }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @user.save!
   end
 
   def update
